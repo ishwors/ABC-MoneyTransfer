a475c0844414963a566f30b10ecab7ffd976e4c6
1shwor <ishwors.aloi@gmail.com>
2025-03-05 09:34:30 +0545

    🎉 Initial commit - Installed required packages

---
+++ MoneyTransfer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoneyTransfer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
